4e3809c0b8f2c44d1f348f38719f487970903e9c
asia-shwan <asiashwan098@gmail.com>
2022-04-07 13:31:33 +0300

    first commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img src="/img/a.jpg" alt="">
+    <h1>Name : Asia Shwan<h1>
+    <h1> Age  : 22year<h1>
+    <h1>stage : stage three<h1>
+    
+    
+</body>
+</html>
